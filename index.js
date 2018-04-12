6dbd0158f215a51f6ed9f159316d5f4a88b3f2b9
Kenny <kenny47201@gmail.com>
2018-04-12 05:05:55 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,84 +1,96 @@
 $(() => {
+  
+  localStorage.clear()
+  let movies = []
+  let tvShows = []
 
-    let loaded = false
-  
-    $('#clearButton').click(() => {
-      $('#tableBody').empty()
-      $('#dataOne').text('Pick')
-      $('#dataTwo').text('From')
-      $('#dataThree').text('Above')
-      loaded = false
-    })
-  
-    $('#peopleButton').click(() => {
-      if(!loaded) {
-        loaded = true
-  
-        $.ajax({
-          type: 'GET',
-          url: 'https://swapi.co/api/people'
-        }).done((res) => {
-          $('#dataOne').text('Name')
-          $('#dataTwo').text('Height')
-          $('#dataThree').text('Birth Year')
-  
-          let people = res.results
-          for(p of people) {
-            $('#tableBody').append(createPersonRow(p))
-          }
-        })
-      }
-    })
-  
-    $('#planetButton').click(() => {
-      if(!loaded){
-        loaded = true
-  
-        $.get('https://swapi.co/api/planets/')
-          .done((res) => {
-            $('#dataOne').text('Name')
-            $('#dataTwo').text('Diameter')
-            $('#dataThree').text('Climate')
-  
-            let planets = res.results
-            for (p of planets) {
-              $('#tableBody').append(createPlanetRow(p))
-            }
-          })
-      }
-    })
-  
-    function createPersonRow(person) {
-      let row = $(`<tr></tr>`)
-      let name = $(`<td>${person.name}</td>`)
-      let height = $(`<td>${person.height}</td>`)
-      let birth = $(`<td>${person.birth_year}</td>`)
-  
-      row.append(name, height, birth)
-  
-      return row
-    }
-  
-    function createPlanetRow(planet) {
-      let row = $(`<tr></tr>`)
-      let name = $(`<td>${planet.name}</td>`)
-      let diameter = $(`<td>${planet.diameter}</td>`)
-      let climate = $(`<td>${planet.climate}</td>`)
-  
-      row.append(name, diameter, climate)
-  
-      return row
-    }
-    
-    $('#searchPersonForm').submit((e) => {
-      e.preventDefault()
-      
-      let input = $('#inputName').val()
-      $('#inputName').val('')  
-      $.get(`https://swapi.co/api/people/?search=${input}`)
-        .done((res) => {
-          let person = res.results[0]
-          $('#personInfoPanel').text(`${person.name}'s hair is color is: ${person.hair_color}`)
-        })
-    })
+  $('#clearButton').click(() => {
+    $('#tableBody').empty()
+    $('#dataOne').text('Pick')
+    $('#dataTwo').text('From')
+    $('#dataThree').text('Above')
   })
+
+  $('#peopleButton').click(() => {
+    $('#tableBody').empty()
+    $('#dataOne').text('Name')
+    $('#dataTwo').text('Height')
+    $('#dataThree').text('Birth Year')
+
+    if(!localStorage.getItem('storedPeople')){
+      $.ajax({
+        type: 'GET',
+        url: 'https://swapi.co/api/people'
+      }).done((res) => {
+        let people = res.results
+        for(p of people) {
+          storedPeople.push({name: p.name, height: p.height, birth_year: p.birth_year})
+          $('#tableBody').append(createPersonRow(p))
+        }
+        localStorage.setItem('storedPeople', JSON.stringify(storedPeople))
+      })
+    } else {
+      for(p of storedPeople) {
+        $('#tableBody').append(createPersonRow(p))
+      }
+    }
+  })
+
+  $('#planetButton').click(() => {
+    $('#tableBody').empty()
+    $('#dataOne').text('Name')
+    $('#dataTwo').text('Diameter')
+    $('#dataThree').text('Climate')
+    
+    if(!localStorage.getItem('storedPlanets')) {
+      $.get('https://swapi.co/api/planets/')
+      .done((res) => {
+        let planets = res.results
+        for(p of planets) {
+          storedPlanets.push({name: p.name, diameter: p.diameter, climate: p.climate})
+          $('#tableBody').append(createPlanetRow(p))
+        }
+        localStorage.setItem('storedPlanets', JSON.stringify(storedPlanets))
+      })
+    } else {
+      for(p of storedPlanets) {
+        $('#tableBody').append(createPlanetRow(p))
+      }
+    }
+  })
+
+  function createPersonRow(person) {
+    let row = $(`<tr></tr>`)
+    let name = $(`<td>${person.name}</td>`)
+    let height = $(`<td>${person.height}</td>`)
+    let birth = $(`<td>${person.birth_year}</td>`)
+
+    row.append(name, height, birth)
+
+    return row
+  }
+
+  function createPlanetRow(planet) {
+    let row = $(`<tr></tr>`)
+    let name = $(`<td>${planet.name}</td>`)
+    let diameter = $(`<td>${planet.diameter}</td>`)
+    let climate = $(`<td>${planet.climate}</td>`)
+
+    row.append(name, diameter, climate)
+
+    return row
+  }
+
+  $('#searchPersonForm').submit((e) => {
+    e.preventDefault()
+    
+    let input = $('#inputName').val()
+    $('#inputName').val('')
+
+    $.get(`https://swapi.co/api/people/?search=${input}`)
+      .done((res) => {
+        let person = res.results[0]
+        $('#personInfoPanel').text(`${person.name}'s hair is: ${person.hair_color}`)
+      })
+  })
+})
